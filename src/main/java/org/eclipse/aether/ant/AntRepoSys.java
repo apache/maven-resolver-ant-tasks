27fa29f4b2e5b143e198ed39a7c1f74d24392340
Benjamin Bentmann <bentmann@sonatype.com>
2012-03-10 17:28:07 +0100

    Accounted for API refactoring done to solve issue #373251

--- src/main/java/org/eclipse/aether/ant/AntRepoSys.java
+++ src/main/java/org/eclipse/aether/ant/AntRepoSys.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Sonatype, Inc.
+ * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -215,9 +215,6 @@ public class AntRepoSys
         configProps.putAll( (Map<?, ?>) project.getProperties() );
         configProps.putAll( (Map<?, ?>) project.getUserProperties() );
         session.setConfigProps( configProps );
-
-        session.setNotFoundCachingEnabled( false );
-        session.setTransferErrorCachingEnabled( false );
 
         session.setOffline( isOffline() );
         session.setUserProps( project.getUserProperties() );

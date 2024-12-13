552a48ac0207b5d415eec07e0cbf39b848c5db2f
jkahrman <jkahrman@users.noreply.github.com>
2024-12-13 15:21:01 -0500

    Don't default the scope of managed dependencies to 'compile'
    If a scope isn't defined in a managed dependency, the scope specified in
    the dependency section is used for resolving transitive dependencies.
    
    I thought that letting the maven-resolver classes do all defaulting
    would have worked, but without defaulting the scope of the other
    dependencies created by ConverterUtils.toDependency,
    ResolveTest.testResolveNestedDependencyCollections() fails. Removing
    this defaulting still seems like it might be the right thing to do, but
    I'm not pulling on this thread. I don't know what it is attached to.

--- src/main/java/org/apache/maven/resolver/internal/ant/ConverterUtils.java
+++ src/main/java/org/apache/maven/resolver/internal/ant/ConverterUtils.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.maven.resolver.internal.ant.types.Authentication;
 import org.apache.maven.resolver.internal.ant.types.Dependency;
 import org.apache.maven.resolver.internal.ant.types.Exclusion;
@@ -82,6 +83,16 @@ class ConverterUtils {
 
     public static org.eclipse.aether.graph.Dependency toDependency(
             Dependency dependency, List<Exclusion> exclusions, RepositorySystemSession session) {
+        String scope = dependency.getScope();
+        return new org.eclipse.aether.graph.Dependency(
+                toArtifact(dependency, session.getArtifactTypeRegistry()),
+                ObjectUtils.isEmpty(scope) ? "compile" : scope,
+                false,
+                toExclusions(dependency.getExclusions(), exclusions));
+    }
+
+    public static org.eclipse.aether.graph.Dependency toManagedDependency(
+        Dependency dependency, List<Exclusion> exclusions, RepositorySystemSession session) {
         return new org.eclipse.aether.graph.Dependency(
                 toArtifact(dependency, session.getArtifactTypeRegistry()),
                 dependency.getScope(),
